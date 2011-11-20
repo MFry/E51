c8d2bfce0ebec213a6ba05a1306b1bfd9ef48ff6
MFry <michal.frystacky@gmail.com>
2011-11-20 15:05:28 -0500

    Made code more readable

--- elevator/Elevator.java
+++ elevator/Elevator.java
@@ -2,42 +2,51 @@ import java.util.HashMap;
 import java.util.PriorityQueue;
 
 public class Elevator {
-
-   // @NOTE: there is not elevator state for up and down
-   // because this is the job of the elevator manager
-
+   
+//TODO need to add a function that allows the manager to add goals for the elevator
+   
    private int floor; // The current floor of the elevator
    private int startingFloor; // The floor the elevator starts on
    private final int maxCap;
    private int distanceTrav;
    private int curCap; // Current Capacity of the elevator
    private int state; // -1 going down, 0 not moving, 1 going up
-   private PriorityQueue<Integer> goals;
-   private HashMap<Integer, Person> contains;
-   public static int DOWN = -1;
-   public static int STATIC = 0;
-   public static int UP = 1;
+   private PriorityQueue<Integer> goals; // The goals for the elevator
+   private HashMap<Integer, Person> contains; // The people the elevator
+                                              // contains
    private FloorComparatorDown down = new FloorComparatorDown();
    private FloorComparatorUp up = new FloorComparatorUp();
+   /* Variables to be used by the manager as well as the elevator */
+   public static final int DOWN = -1;
+   public static final int STATIC = 0;
+   public static final int UP = 1;
 
    public Elevator(int maxCap, int start) {
+      // TODO need to keep in mind a maximum floor an elevator can reach
+      /***
+       * The constructor for an elevator, with an initial starting floor and
+       * maximum capacity
+       */
       this.maxCap = maxCap;
       this.distanceTrav = 0;
       this.startingFloor = start;
+      this.state = STATIC; // The elevator hasn't moved yet
       goals = new PriorityQueue<Integer>();
       contains = new HashMap<Integer, Person>(maxCap);
    }
 
    public int getCurCap() {
-      // returns the current capacity of the elevator
+      /*** returns the current capacity of the elevator */
       return curCap;
    }
 
    public void setCurCap(int people) {
+      /*** Changes the current capacity of the elevator */
       this.curCap = people;
    }
 
    public boolean changeState(int newState) {
+      /*** Changes whether the elevator goes up or down */
       if (newState != state) {
          if (goals.isEmpty()) {
             state = newState;
@@ -49,6 +58,10 @@ public class Elevator {
    }
 
    private void setState(int newState) {
+      /***
+       * deletes the old goals Queue and changes it to the appropriate one so
+       * that it priorities based on the state
+       */
       if (newState > 0) {
          goals = new PriorityQueue<Integer>(2 * maxCap, up);
       } else {
@@ -57,14 +70,17 @@ public class Elevator {
    }
 
    public int getDistance() {
+      /*** Returns the total distance the elevator has traveled */
       return distanceTrav;
    }
 
    public int getState() {
+      /*** Returns the direction the elevator will move */
       return state;
    }
 
    public boolean isActive() {
+      /*** returns whether the elevator has moved in the last unit time */
       if (state != 0) {
          return true;
       }
@@ -72,9 +88,11 @@ public class Elevator {
    }
 
    private boolean checkValid(int f) {
-      // Takes the input f (floor) and check if its a valid floor
-      // Example: if f is somewhere below the current floor of the elevator but
-      // the elevator is going up it will be rejected
+      /***
+       * Takes the input f (floor) and check if its a valid floor.
+       * Example: if f is somewhere below the current floor of 
+       * the elevator but the elevator is going up it will be rejected
+       */
 
       if (f < floor && state == UP) {
          return false;
@@ -85,9 +103,10 @@ public class Elevator {
    }
 
    public boolean enter(Person p) {
-      // Adds a person when appropriate inside the elevator
-      // keeping track of necessary information and without losing the instance
-      // of the person
+      /***
+       * Adds a person when appropriate inside the elevator keeping track of
+       * necessary information and without losing the instance of the person
+       */
 
       if (curCap == maxCap) {
          return false;
@@ -101,6 +120,7 @@ public class Elevator {
    }
 
    public Person update() {
+      /*** moves the elevator forward one unit of time */
       if (!goals.isEmpty()) {
          floor += state;
          distanceTrav += state;
