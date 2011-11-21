a7e077368a7e8569c63f44aea1d41bb451ac986d
MFry <michal.frystacky@gmail.com>
2011-11-21 11:42:57 -0500

    Fixed the goals bug

--- elevator/Elevator.java
+++ elevator/Elevator.java
@@ -6,14 +6,15 @@ public class Elevator {
 
    private int floor; // The current floor of the elevator
    private int startingFloor; // The floor the elevator starts on
-   private final int maxCap; //Maximum Capacity of the elevator
-   private final int maxFloor; //The maximum Floor the elevator can reach
+   private final int maxCap; // Maximum Capacity of the elevator
+   private final int maxFloor; // The maximum Floor the elevator can reach
    private int distanceTrav;
    private int curCap; // Current Capacity of the elevator
    private int state; // -1 going down, 0 not moving, 1 going up
    private PriorityQueue<Integer> goals; // The goals for the elevator
-   private HashMap<Integer, LinkedList<Person>> contains; // The people the elevator
-                                              // contains
+   private HashMap<Integer, LinkedList<Person>> contains; // The people the
+                                                          // elevator
+   // contains
    private FloorComparatorDown down = new FloorComparatorDown();
    private FloorComparatorUp up = new FloorComparatorUp();
    /* Variables to be used by the manager as well as the elevator */
@@ -21,8 +22,9 @@ public class Elevator {
    public static final int STATIC = 0;
    public static final int UP = 1;
 
-   //TODO Update the elevator software so that it automatically switches states if it hits maxFloor
-   
+   // TODO Update the elevator software so that it automatically switches states
+   // if it hits maxFloor
+
    public Elevator(int maxCap, int start, int upperElevatorRange) {
       /***
        * The constructor for an elevator, with an initial starting floor and
@@ -128,7 +130,7 @@ public class Elevator {
       goals.add(floorWanted);
       LinkedList<Person> group = contains.get(floorWanted);
       if (group != null) {
-         //group exists so we add the person to the existing group
+         // group exists so we add the person to the existing group
          group.add(p);
       } else {
          // group doesnt exist so we create a group and add its first person
@@ -140,36 +142,40 @@ public class Elevator {
       return true;
    }
 
-   private void checkRange () {
+   private void checkRange() {
       /***
-       * Updates the elevator's state 
-       * should the elevator reach the max floor
+       * Updates the elevator's state should the elevator reach the max floor
        */
       if (floor == maxFloor) {
-         assert !goals.isEmpty(); //If the goals are not empty this is a bug
-         setState (state * -1);
+         assert !goals.isEmpty(); // If the goals are not empty this is a bug
+         setState(state * -1);
       }
    }
-   
+
    public LinkedList<Person> update() {
       /***
-       * Moves the elevator forward one unit of time Returns either a person or
-       * null, should null be returned it means that no person left, but people might be entering.
+       * Moves the elevator forward one unit of time. The elevator moves up or
+       * down a floor depending on its state and if it has a goal. Returns
+       * either a person or null, should null be returned it means that no
+       * person left that floor, but people might be entering.
        */
-       
-       // TODO Fix bug on removing more than one element in goals
+
       if (!goals.isEmpty()) {
          floor += state;
          distanceTrav += state;
-         checkRange ();
+         checkRange();
          int localGoal = goals.peek();
          // We have found a goal set
          if (localGoal == floor) {
-            //int key = goals.remove();
-            goals.remove ();
+            // int key = goals.remove(); //Good idea by Roger
+            while (localGoal == floor) {
+               goals.remove();
+               localGoal = goals.remove();
+            }
             LinkedList<Person> peopleLeaving = contains.get(localGoal);
-            return peopleLeaving;
-         }    
+            return peopleLeaving; // Returns the group of people departing on
+                                  // this floor
+         }
       }
       state = STATIC; // we have nothing to do
       return null;
