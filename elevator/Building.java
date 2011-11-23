40728519d1ef348b1eba083d06bd937380e4c168
Roger Cotrina <rcotrina2010@my.fit.edu>
2011-11-23 13:47:15 -0500

    Updated two methods for insertion and remove in specific floors

--- elevator/Building.java
+++ elevator/Building.java
@@ -8,13 +8,17 @@ public class Building {
    
     // Array of two arrays that have a 
     int numbFloors;
+    public static final int UP = 0;
+    public static final int DOWN = 1;
+    public static final int STATIC = 2;
     
     @SuppressWarnings("unchecked")
     public Building(int numFloors) {
         numbFloors = numFloors;
-        floors = (ArrayList<Person>[][]) new ArrayList[numFloors][2];
-        // 0 is arraylist of people going down
-        // 1 is arralist of people going up
+        floors = (ArrayList<Person>[][]) new ArrayList[numFloors][3];
+        // 0 is array list of people going down
+        // 1 is array list of people going up
+        // 2 is array list of people staying in the floor
     }
 
     
@@ -25,21 +29,36 @@ public class Building {
         return floors[floor][state].size ();
     }
     
-    public void insert(Person some){
+    public void enterBuilding(Person some){
         // Inserts the person on the specific floor and queue of either going up
         // or down
         if(some.getDirection() == 1){ // Going up
-            floors[some.getDestinationFloor()][0].add (some);
+            floors[0][UP].add (some);
         }
         else if (some.getDirection() == -1){ // Going down
-            floors[some.getDestinationFloor()][1].add(some);
+            floors[0][DOWN].add(some);
+        } else {
+            floors[0][STATIC].add (some);
         }
+    }
+    
+    // Not sure
+    public void insertInFloor(int floor, Person some){
+        // remove first
+        floors[floor][STATIC].add (some);
+    }
+    // int currentFloor, int state, Integer destination
+    public Person removeFromFloor(int currentFloor, int state, Integer destination){
+        // find person in that floor.
+        //Person removed = new Person()
+        return floors[currentFloor][state].remove (0);
     }
     
     public Person remove(Person some){
         Person test;
+        // People going up
         if(some.getDirection() == 1){
-            test = floors[some.getDestinationFloor()][0].remove (floors[some.getDestinationFloor()][0].indexOf (some));
+            test = floors[some.getDestinationFloor()][UP].remove (floors[some.getDestinationFloor()][0].indexOf (some));
             return test;
         }
         else {
