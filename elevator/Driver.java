420ba96b9a5bccd0fa48514fa93ecfdc57005134
Roger Cotrina <rcotrina2010@my.fit.edu>
2011-11-24 12:49:55 -0500

    -Updated Person.java method name and started working with the driver.

--- elevator/Driver.java
+++ elevator/Driver.java
@@ -1,4 +1,8 @@
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * 
@@ -6,35 +10,52 @@ import java.util.ArrayList;
  *
  */
 public class Driver {
-    
+
     // Create a method that reads input from Joe's output
     // For each person, if they are inserted, create as a new person.
     // Then insert into the building accordingly
     public static int idGenerator = 1;
-    public static void main(String[] args){
-        Building building = new Building(10);
-        ArrayList<Person> people = new ArrayList<Person>();
-        people.add(new Person(idGenerator++, 0, 10, 1));
-        people.add(new Person(idGenerator++, 0, 10, 1));
-        people.add(new Person(idGenerator++, 0, 10, 1));
-        people.add (new Person(idGenerator++, 0, 10, 1));
-        people.add (new Person(idGenerator++, 0, 10, 1));
-        people.add(new Person(idGenerator++, 0, 10, 1));
-        people.add (new Person(idGenerator++, 0, 10, 1));
-        people.add(new Person(idGenerator++, 0, 10, 1));
-        people.add (new Person(idGenerator++, 1, 10, 1));
-        people.add(new Person(idGenerator++, 1, 10, 1));
-        people.add(new Person(idGenerator++, 2, 10, 1));
-        people.add (new Person(idGenerator++, 2, 10, 1));
-        people.add (new Person(idGenerator++, 3, 10, 1));
-        people.add(new Person(idGenerator++, 4, 10, 1));
-        people.add (new Person(idGenerator++, 7, 10, 1));
-        people.add(new Person(idGenerator++, 8, 10, 1));
-        people.add (new Person(idGenerator++, 7, 10, 1));
-        people.add (new Person(idGenerator++, 9, 10, 1));
-        for(Person x: people){
-            building.enterBuilding (x);
+
+    public static void main (String[] args) {
+        Building building = new Building (10, 2);
+        /**
+         * TODO For every time unit the manager should do something
+         * Check according to the state, if person has to be created or moved.
+         */
+        try {
+            // Open the file that is the first
+            // command line parameter
+            FileInputStream fstream = new FileInputStream ("textfile.txt");
+            // Get the object of DataInputStream
+            DataInputStream in = new DataInputStream (fstream);
+            BufferedReader br = new BufferedReader (new InputStreamReader (in));
+            String strLine;
+            Scanner scan;
+            String movement;
+            int time;
+            int destFloor;
+            int direction;
+            // Read File Line By Line
+            while ( ( strLine = br.readLine ()) != null) {
+                // Manage for every single time
+                scan = new Scanner(strLine);
+                time = scan.nextInt ();
+                movement = scan.next ();
+                destFloor = scan.nextInt ();
+                direction = scan.nextInt ();
+                
+                if (movement.equals ("create")){
+                    building.enterBuilding (new Person(idGenerator++, time, destFloor, direction));
+                }
+                else {
+                    // do movements within floors throughout the elevator manager
+                }
+            }
+            // Close the input stream
+            in.close ();
+        } catch (Exception e) {// Catch exception if any
+            System.err.println ("Error: " + e.getMessage ());
         }
     }
-    
+
 }
