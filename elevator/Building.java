46c03d45c95a5db6296b0a5bacd8df8601982c7f
MFry <michal.frystacky@gmail.com>
2011-11-25 12:23:17 -0500

    Fixing Roger's horrendous documentation

--- elevator/Building.java
+++ elevator/Building.java
@@ -1,89 +1,90 @@
 import java.util.ArrayList;
 
 public class Building {
-    // LinkedList<Person>[] floors;
+   // LinkedList<Person>[] floors;
 
-    ArrayList<Person> floors[][];
+   // A two dimensional array structured as:
+   // [Building Floors][People Going Up/Down/Staying] containing linked lists of
+   // people
+   ArrayList<Person> floors[][];
 
-    // Array of two arrays that have a
-    int numbFloors;
-    int numbElevators;
-    public static final int UP = 0;
-    public static final int DOWN = 1;
-    public static final int STATIC = 2;
+   int numbFloors;
+   int numbElevators;
+   public static final int UP = 0;
+   public static final int DOWN = 1;
+   public static final int STATIC = 2;
 
-    @SuppressWarnings("unchecked")
-    public Building(int numFloors, int numElevators) {
-        numbFloors = numFloors;
-        numbElevators = numElevators;
-        floors = (ArrayList<Person>[][]) new ArrayList[numFloors][3];
-        for (int i = 0; i < floors.length; i++) {
-            floors[i][0] = new ArrayList<Person> ();
-            floors[i][1] = new ArrayList<Person> ();
-            floors[i][2] = new ArrayList<Person> ();
-        }
+   @SuppressWarnings("unchecked")
+   public Building(int numFloors, int numElevators) {
+      numbFloors = numFloors;
+      numbElevators = numElevators;
+      floors = (ArrayList<Person>[][]) new ArrayList[numFloors][3];
+      for (int i = 0; i < floors.length; i++) {
+         floors[i][0] = new ArrayList<Person>();
+         floors[i][1] = new ArrayList<Person>();
+         floors[i][2] = new ArrayList<Person>();
+      }
 
-        // 0 is array list of people going down
-        // 1 is array list of people going up
-        // 2 is array list of people staying in the floor
-    }
+      // 0 is array list of people going down
+      // 1 is array list of people going up
+      // 2 is array list of people staying in the floor
+   }
 
-    public int getPeople (int floor, int state) {
-        /**
-         * Returns the number of people in a floor wishing to go either up or
-         * down
-         */
-        return floors[floor][state].size ();
-    }
+   public int getPeople(int floor, int state) {
+      /**
+       * Returns the number of people in a floor wishing to go either up or down
+       */
+      return floors[floor][state].size();
+   }
 
-    public void enterBuilding (Person some) {
-        // Inserts the person on the specific floor and queue of either going up
-        // or down
-        if (some.getDirection () == 1) { // Going up
-            floors[0][UP].add (some);
-        } else if (some.getDirection () == -1) { // Going down
-            floors[0][DOWN].add (some);
-        } else {
-            floors[0][STATIC].add (some);
-        }
-    }
+   public void enterBuilding(Person some) {
+      // Inserts the person on the specific floor and queue of either going up
+      // or down
+      if (some.getDirection() == 1) { // Going up
+         floors[0][UP].add(some);
+      } else if (some.getDirection() == -1) { // Going down
+         floors[0][DOWN].add(some);
+      } else {
+         floors[0][STATIC].add(some);
+      }
+   }
 
-    // Not sure
-    public void insertInFloor (int floor, Person some) {
-        // remove first
-        floors[floor][STATIC].add (some);
-    }
+   // Not sure
+   public void insertInFloor(int floor, Person some) {
+      // remove first
+      floors[floor][STATIC].add(some);
+   }
 
-    // int currentFloor, int state, Integer destination
-    public Person removeFromFloor (int currentFloor, int state,
-            Integer destination) {
-        // find person in that floor.
-        // Person removed = new Person()
-        return floors[currentFloor][state].remove (0);
-    }
+   // int currentFloor, int state, Integer destination
+   public Person removeFromFloor(int currentFloor, int state,
+         Integer destination) {
+      // find person in that floor.
+      // Person removed = new Person()
+      return floors[currentFloor][state].remove(0);
+   }
 
-    // Removes an specific person from its proper queue
-    public Person remove (Person some) {
-        Person test = null;
-        if (some.getDirection () == 1) {
-            test = floors[some.getDestinationFloor ()][UP].remove (floors[some
-                    .getDestinationFloor ()][UP].indexOf (some));
-        } else if (some.getDirection () == -1){
-            test = floors[some.getDestinationFloor ()][DOWN].remove (floors[some
-                    .getDestinationFloor ()][DOWN].indexOf (some));
-        }
-        return test;
-    }
+   // Removes an specific person from its proper queue
+   public Person remove(Person some) {
+      Person test = null;
+      if (some.getDirection() == 1) {
+         test = floors[some.getDestinationFloor()][UP].remove(floors[some
+               .getDestinationFloor()][UP].indexOf(some));
+      } else if (some.getDirection() == -1) {
+         test = floors[some.getDestinationFloor()][DOWN].remove(floors[some
+               .getDestinationFloor()][DOWN].indexOf(some));
+      }
+      return test;
+   }
 
-    // Removes the first person on the queue according to its direction
-    public Person remove (int floor, int direction) {
-        Person test;
-        test = floors[floor][direction].remove (0);
-        return test;
-    }
+   // Removes the first person on the queue according to its direction
+   public Person remove(int floor, int direction) {
+      Person test;
+      test = floors[floor][direction].remove(0);
+      return test;
+   }
 
-    public int getNumElevators () {
-        return numbElevators;
-    }
+   public int getNumElevators() {
+      return numbElevators;
+   }
 
 }
