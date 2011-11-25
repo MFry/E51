824a943df5b40774878d5576122d200d108adb5a
Kim Day <kday2009@my.fit.edu>
2011-11-25 17:24:36 -0500

    Oh no not this problem again

--- ElevatorGUI/src/Elevator.java
+++ ElevatorGUI/src/Elevator.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 
-public class Elevator implements Comparable<Elevator>{
+public class Elevator implements Comparable<Elevator> {
 
    private int floor; // The current floor of the elevator
    private int startingFloor; // The floor the elevator starts on
@@ -13,25 +13,36 @@ public class Elevator implements Comparable<Elevator>{
    private int state; // -1 going down, 0 not moving, 1 going up
    private PriorityQueue<Integer> goals; // The goals for the elevator
    private HashMap<Integer, LinkedList<Person>> contains; // The people the
-   /*STATE*/                                              // elevator
+   /* STATE */// elevator
    private boolean dumbMode;
    // contains
-   private FloorComparatorDescending down = new FloorComparatorDescending();
    private FloorComparatorAscending up = new FloorComparatorAscending();
+   private FloorComparatorDescending down = new FloorComparatorDescending();
    /* Variables to be used by the manager as well as the elevator */
    public static final int DOWN = -1;
    public static final int STATIC = 0;
    public static final int UP = 1;
-   
+
    private static final char dMode = 'd';
+   
    // TODO Update the elevator software so that it automatically switches states
    // if it hits maxFloor
 
+   /***
+    * The constructor for an elevator, with an initial starting floor and
+    * maximum capacity
+    * 
+    * @param maxCap
+    *           Sets the maximum capacity for the elevator (total people)
+    * @param start
+    * 
+    * @param upperElevatorRange
+    * 
+    * @param mode
+    * 
+    */
    public Elevator(int maxCap, int start, int upperElevatorRange, String mode) {
-      /***
-       * The constructor for an elevator, with an initial starting floor and
-       * maximum capacity
-       */
+
       this.maxCap = maxCap;
       this.maxFloor = upperElevatorRange;
       this.distanceTrav = 0;
@@ -40,34 +51,45 @@ public class Elevator implements Comparable<Elevator>{
       this.state = STATIC; // The elevator hasn't moved yet
       goals = new PriorityQueue<Integer>();
       contains = new HashMap<Integer, LinkedList<Person>>(maxCap);
-      extractMode (mode);
+      extractMode(mode);
    }
 
-   private void extractMode (String mode) {
-      /***
-       * Sets the appropriate modes
-       * d - for dumbMode
-       */
-      //currently only one state
+   /***
+    * Sets the appropriate modes for the elevator d - for dumbMode
+    * 
+    * @param mode
+    *           A string that contains the modes with no spaces
+    */
+   private void extractMode(String mode) {
+
+      // currently only one state
       for (int i = 0; i < mode.length(); ++i) {
          if (mode.charAt(i) == dMode) {
             dumbMode = true;
          }
       }
    }
-   
+
+   /*** @returns the current capacity of the elevator */
    public int getCurCap() {
-      /*** returns the current capacity of the elevator */
       return curCap;
    }
 
+   /***
+    * Changes the current capacity of the elevator
+    * 
+    * @param people
+    *           Total number of people currently in the elevator
+    */
    public void setCurCap(int people) {
-      /*** Changes the current capacity of the elevator */
       this.curCap = people;
    }
 
+   /***
+    * Changes whether the elevator goes up or down
+    */
    public boolean changeState(int newState) {
-      /*** Changes whether the elevator goes up or down */
+
       if (newState != state) {
          if (goals.isEmpty()) {
             state = newState;
@@ -78,61 +100,72 @@ public class Elevator implements Comparable<Elevator>{
       return false;
    }
 
+   /***
+    * deletes the old goals Queue and changes it to the appropriate one so that
+    * it priorities based on the new state of the elevator
+    */
    private void setState(int newState) {
-      /***
-       * deletes the old goals Queue and changes it to the appropriate one so
-       * that it priorities based on the state
-       */
+
       if (newState > 0) {
-         goals = new PriorityQueue<Integer>(2 * maxCap, down);
-      } else {
          goals = new PriorityQueue<Integer>(2 * maxCap, up);
+      } else {
+         goals = new PriorityQueue<Integer>(2 * maxCap, down);
       }
    }
 
+   /***
+    * Sets a goal for which floor the elevator will stops on
+    * 
+    * @param toDo
+    *           A floor that the elevator will go to
+    */
    public void setGoal(int toDo) {
-      /***
-       * Sets a goal for which the floor the elevator stops on
-       */
       goals.add(toDo);
    }
 
+   /*** @Returns the total distance the elevator has traveled */
    public int getDistance() {
-      /*** Returns the total distance the elevator has traveled */
       return distanceTrav;
    }
 
+   /*** @Returns the direction the elevator will move */
    public int getState() {
-      /*** Returns the direction the elevator will move */
+
       return state;
    }
 
-   public boolean isFull () {
+   /*** @returns whether the elevator is full or not */
+   public boolean isFull() {
       return maxCap == curCap;
    }
-   
-   public int getCurrentFloor () {
-      /***
-       * Returns the floor the elevator is currently on
-       */
+
+   /*** Changes the current floor of the elevator */
+   // TODO not needed
+   /*
+    * public void setCurrentFloor(int floor) { this.floor = floor; }
+    */
+   /***
+    * @Returns The floor the elevator is currently on
+    */
+   public int getCurrentFloor() {
       return this.floor;
    }
-   
+
+   /*** @Returns whether the elevator has moved in the last unit time */
    public boolean isActive() {
-      /*** returns whether the elevator has moved in the last unit time */
+
       if (state != 0) {
          return true;
       }
       return false;
    }
 
+   /***
+    * Takes the input f (floor) and check if its a valid floor. Example: if f is
+    * somewhere below the current floor of the elevator but the elevator is
+    * going up it will be rejected
+    */
    private boolean checkValid(int f) {
-      /***
-       * Takes the input f (floor) and check if its a valid floor. Example: if f
-       * is somewhere below the current floor of the elevator but the elevator
-       * is going up it will be rejected
-       */
-
       if (f < floor && state == UP) {
          return false;
       } else if (f > floor && state == DOWN) {
@@ -140,14 +173,14 @@ public class Elevator implements Comparable<Elevator>{
       }
       return true;
    }
-   
-   //TODO Write a more specific method for enter thats takes in a certain person
-   
+
+   // TODO Write a more specific method for enter thats takes in a certain
+   // person
+   /***
+    * Adds a person when appropriate inside the elevator keeping track of
+    * necessary information and without losing the instance of the person
+    */
    public boolean enter(Person p) {
-      /***
-       * Adds a person when appropriate inside the elevator keeping track of
-       * necessary information and without losing the instance of the person
-       */
 
       if (curCap == maxCap) {
          return false;
@@ -171,38 +204,48 @@ public class Elevator implements Comparable<Elevator>{
       return true;
    }
 
+   /***
+    * Updates the elevator's state should the elevator reach the max floor
+    */
    private void checkRange() {
-      /***
-       * Updates the elevator's state should the elevator reach the max floor
-       */
+
       if (floor == maxFloor) {
          assert !goals.isEmpty(); // If the goals are not empty this is a bug
          setState(state * -1);
       }
    }
 
+   /***
+    * Moves the elevator forward one unit of time. The elevator moves up or down
+    * a floor depending on its state and if it has a goal. Returns either a
+    * group of people or null, should null be returned it means that no person
+    * left that floor, but people might be entering.
+    */
    public LinkedList<Person> update() {
-      //TODO Check what floors the elevator can move to
-      /***
-       * Moves the elevator forward one unit of time. The elevator moves up or
-       * down a floor depending on its state and if it has a goal. Returns
-       * either a group of people or null, should null be returned it means that no
-       * person left that floor, but people might be entering.
-       */
-
+      // TODO Check what floors the elevator can move to
       if (!goals.isEmpty()) {
          floor += state;
-         distanceTrav += state;
+         distanceTrav += 1;
          checkRange();
+         int destination = goals.peek();
          int localGoal = goals.peek();
          // We have found a goal set
          if (localGoal == floor) {
             // int key = goals.remove(); //Good idea by Roger
             while (localGoal == floor) {
                goals.remove();
-               localGoal = goals.remove();
+               localGoal = goals.peek();
             }
-            LinkedList<Person> peopleLeaving = contains.get(localGoal);
+            LinkedList<Person> peopleLeaving = contains.get(destination); // TODO
+                                                                          // change
+                                                                          // name
+                                                                          // of
+                                                                          // destination
+                                                                          // to
+                                                                          // idk
+            if (peopleLeaving != null) {
+               this.curCap -= peopleLeaving.size();
+            }
             return peopleLeaving; // Returns the group of people departing on
                                   // this floor
          }
@@ -213,17 +256,29 @@ public class Elevator implements Comparable<Elevator>{
       return null;
    }
 
+   /***
+    * Allows elevators to be comparable by priority (which is the current floor
+    * they are on)
+    */
    @Override
    public int compareTo(Elevator o) {
-      /***
-       * Allows elevators to be comparable by priority (which is the current floor they are on)
-       */
-      //TODO Test this function
-      if (this.floor > o.floor ) {
+
+      // TODO Test this function
+      if (this.floor > o.floor) {
          return 1;
-      } else if (this.floor < o.floor ) {
+      } else if (this.floor < o.floor) {
          return -1;
       }
       return 0;
+   }
+   
+   /**
+    * Gets the elevator's status
+    * @return Array containing: Floor, starting floor,
+    * maximum capacity, maximum floor, distance traveled, current capacity, state
+    */
+   public int[] getInfo () {
+      int[] info = {floor, startingFloor, maxCap, maxFloor, distanceTrav, curCap, state};
+      return info;
    }
 }
